b3eb38ae96a781822fca8d922fe25f778443ff56
Gene Hsu <gene.hsu@utexas.edu>
2018-04-20 16:31:51 -0500

    added hack form

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Google-Calendar-Phishing
 Ethical Hacking final project
+EDUCATIONAL PURPOSES ONLY
+This project was meant to demonstrate proof of concept of a phishing attack.
+It should not be used as an actual phishing attack.
+
--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 from flask import Flask
+from flask import jsonify
+from flask import request
+from flask import render_template
+import os
+from os.path import join, dirname
+import requests
+import google.oauth2.credentials
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
 from datetime import datetime
 app = Flask(__name__)
 
@@ -12,5 +21,27 @@ def homepage():
     <img src="http://loremflickr.com/600/400" />
     """.format(time=the_time)
 
+@app.route('/hack', methods=['GET', 'POST'])
+def hack_form():
+    if request.method == 'POST':
+        sender = request.form.get('sender')
+        recipient = request.form.get('recipient')
+        return send_initial_email(sender, recipient)
+    else:
+        return render_template('hack_form.html')
+    
+
+def send_initial_email(sender, recipient):
+    return render_template('hack_form.html')    
+
+
+
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
+    # When running locally, disable OAuthlib's HTTPs verification.
+    # ACTION ITEM for developers:
+    #     When running in production *do not* leave this option enabled.
+    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
+    # Specify a hostname and port that are set as a valid redirect URI
+    # for your API project in the Google API Console.
+    app.run('localhost', 8080, debug=True)
