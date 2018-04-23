a092219aa94ec7dcd5c58fd42e9d2fd9f37a2b17
Gene Hsu <gene.hsu@utexas.edu>
2018-04-23 15:46:17 -0500

    fixed indentation

--- models.py
+++ models.py
@@ -3,30 +3,30 @@ import datetime
 from sqlalchemy import inspect
 
 def object_as_dict(obj):
-	return {c.key: getattr(obj, c.key)
+    return {c.key: getattr(obj, c.key)
         for c in inspect(obj).mapper.column_attrs}
-
+        
 class User(db.Model):
-	__tablename__ = 'users'
+    __tablename__ = 'users'
 
-	id = db.Column(db.Integer, primary_key=True)
-	email = db.Column(db.String(120), unique=True, nullable=False)
-	name = db.Column(db.String(120), nullable=True)
-	token = db.Column(db.Text, nullable=True)
-	refresh_token = db.Column(db.Text, nullable=True)
-	token_uri = db.Column(db.Text, nullable=True)
-	client_id = db.Column(db.Text, nullable=True)
-	client_secret = db.Column(db.Text, nullable=True)
-	scopes = db.Column(db.Text, nullable=True)
-	email_sent = db.Column(db.Boolean, default=False)
-	emails = db.relationship('Email', backref='users', lazy=True)
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    name = db.Column(db.String(120), nullable=True)
+    token = db.Column(db.Text, nullable=True)
+    refresh_token = db.Column(db.Text, nullable=True)
+    token_uri = db.Column(db.Text, nullable=True)
+    client_id = db.Column(db.Text, nullable=True)
+    client_secret = db.Column(db.Text, nullable=True)
+    scopes = db.Column(db.Text, nullable=True)
+    email_sent = db.Column(db.Boolean, default=False)
+    emails = db.relationship('Email', backref='users', lazy=True)
 
 
 class Email(db.Model):
-	__tablename__ = 'emails'
+    __tablename__ = 'emails'
 
-	id = db.Column(db.Integer, primary_key=True)
-	message_id = db.Column(db.Text, nullable=False)
-	sender_email = db.Column(db.String(120), nullable=False)
-	body = db.Column(db.Text, nullable=False)
-	user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    message_id = db.Column(db.Text, nullable=False)
+    sender_email = db.Column(db.String(120), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
